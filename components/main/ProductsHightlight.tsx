a87626843bbc92be81bacdbf8b02cf87e0440540
VPA2103 <voan0773@gmail.com>
2026-03-09 21:50:07 +0700

    chinh sua  products feature

--- components/main/ProductsHightlight.tsx
+++ components/main/ProductsHightlight.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import { useRef } from "react";
 import ProductFeature from "../products/ProductFeature";
-import type { NavigationOptions } from "swiper/types";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
@@ -13,9 +11,6 @@ import "swiper/css/navigation";
 import { products_feature } from "@/data/products";
 
 export default function ProductsHighlight() {
-  const prevRef = useRef<HTMLButtonElement | null>(null);
-  const nextRef = useRef<HTMLButtonElement | null>(null);
-
   const highlightProducts = products_feature.filter((p) => p.featured);
 
   return (
@@ -31,24 +26,7 @@ export default function ProductsHighlight() {
           spaceBetween={24}
           grabCursor
           loop
-          onInit={(swiper) => {
-            if (
-              swiper.params.navigation &&
-              typeof swiper.params.navigation !== "boolean"
-            ) {
-              const navigation = swiper.params.navigation as NavigationOptions;
-
-              navigation.prevEl = prevRef.current;
-              navigation.nextEl = nextRef.current;
-
-              swiper.navigation.init();
-              swiper.navigation.update();
-            }
-          }}
-          navigation={{
-            prevEl: prevRef.current,
-            nextEl: nextRef.current,
-          }}
+          navigation   // ← chỉ cần dòng này
           breakpoints={{
             320: { slidesPerView: 1.2 },
             640: { slidesPerView: 2 },
@@ -62,27 +40,6 @@ export default function ProductsHighlight() {
             </SwiperSlide>
           ))}
         </Swiper>
-
-        <div className="flex justify-center gap-6 mt-8">
-          <div>
-            <button
-              ref={prevRef}
-              className="px-4 py-2 border rounded-lg hover:bg-gray-100"
-            >
-              ←
-            </button>
-          </div>
-
-          <div>
-            <button
-              ref={nextRef}
-              className="px-4 py-2 border rounded-lg hover:bg-gray-100"
-            >
-              →
-            </button>
-          </div>
-
-        </div>
 
       </div>
     </section>
