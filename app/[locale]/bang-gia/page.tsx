b3e13ce44e9e9fc6f88ef93f63242ad984f8d49c
VPA2103 <voan0773@gmail.com>
2026-03-26 16:12:43 +0700

    thay doi tieu de của bảng giá

--- app/[locale]/bang-gia/page.tsx
+++ app/[locale]/bang-gia/page.tsx
@@ -1,6 +1,6 @@
 import { useTranslations } from "next-intl";
 import PriceTable from "./PriceTable";
-import { industrialPrices, agriculturePrices,aquaculturePrices } from "@/data/priceData";
+import { industrialPrices, agriculturePrices, aquaculturePrices } from "@/data/priceData";
 
 export default function Page() {
   const t = useTranslations("pricePage");
@@ -10,7 +10,14 @@ export default function Page() {
       <h1 className="text-3xl font-bold text-center text-[--text-color] mb-10 uppercase">
         {t("title")}
       </h1>
-
+      <section className="mb-16">
+        <h2 className="text-xl font-semibold mb-4 text-[--text-color] border-b-2 border-green-200 pb-2">
+          {t("section2Title")}
+        </h2>
+        <div className="max-w-7xl mx-auto py-10">
+          <PriceTable products={agriculturePrices} descriptionNs="agriculture" />
+        </div>
+      </section>
       <section className="mb-16">
         <h2 className="text-xl font-semibold mb-4 text-[--text-color] border-b-2 border-blue-200 pb-2">
           {t("section1Title")}
@@ -20,14 +27,6 @@ export default function Page() {
         </div>
       </section>
 
-      <section className="mb-16">
-        <h2 className="text-xl font-semibold mb-4 text-[--text-color] border-b-2 border-green-200 pb-2">
-          {t("section2Title")}
-        </h2>
-        <div className="max-w-7xl mx-auto py-10">
-          <PriceTable products={agriculturePrices} descriptionNs="agriculture" />
-        </div>
-      </section>
       <section className="mb-16">
         <h2 className="text-xl font-semibold mb-4 text-[--text-color] border-b-2 border-green-200 pb-2">
           {t("section3Title")}
